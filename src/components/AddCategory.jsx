fa65d5115f99ae089b5ea6a16e309a5a2930d433
ImJars <ra374885@uaeh.edu.mx>
2022-08-07 21:32:31 -0500

    Archivo de barril

--- src/components/AddCategory.jsx
+++ src/components/AddCategory.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 
-const AddCategory = ({onNewCategory}) => {
+export const AddCategory = ({onNewCategory}) => {
 
     const [inputValue, setInputValue] = useState('')
 
